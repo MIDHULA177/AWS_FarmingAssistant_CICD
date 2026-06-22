68a25e4086eb96475deab72ad60666e1075af5b0
Midhula <midhulav17r@gmail.com>
2026-06-22 17:32:03 +0530

    add netlify config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:5000'
+    BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:5000',
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000'
   }
 }
